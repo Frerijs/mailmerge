6c87d4db350cc45428d1d2a7b44e67ac7a00f045
Frerijs <edijs.freijs@gmail.com>
2024-12-10 11:15:44 +0200

    Update mailmerge.py

--- mailmerge.py
+++ mailmerge.py
@@ -44,13 +44,15 @@ def perform_mail_merge_single_doc(template_path, csv_data, output_path):
                 # Definējam gan {{key}}, gan {[key]} formātus
                 placeholders = [f'{{{{{key}}}}}', f'{{[{key}]}}']
                 for placeholder in placeholders:
-                    for run in paragraph.runs:
-                        if placeholder in run.text:
-                            # Visus NaN jau ir aizvietoti ar "nav", bet vēlreiz pārbaudām drošībai
-                            replacement = str(value)
-                            run.text = run.text.replace(placeholder, replacement)
-                            # Pievienojam diagnostikas ziņojumu
-                            st.write(f"Aizvietots `{placeholder}` ar `{replacement}`")
+                    if placeholder in paragraph.text:
+                        # Visus NaN jau ir aizvietoti ar "nav", bet vēlreiz pārbaudām drošībai
+                        replacement = str(value)
+                        paragraph.text = paragraph.text.replace(placeholder, replacement)
+                        # Pievienojam diagnostikas ziņojumu
+                        st.write(f"Aizvietots `{placeholder}` ar `{replacement}`")
+                    else:
+                        # Pievienojam diagnostikas ziņojumu, ja vietturs netiek atrasts
+                        st.write(f"Vietturs `{placeholder}` netika atrasts paragrafā.")
 
         # Pievienojam lappuses pārtraukumu, ja nav pirmais ieraksts
         if not first_record:
@@ -189,3 +191,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+	
+	
+	mail merge daļēji darbojas
